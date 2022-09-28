f06872f120b68d624561fd34062488c146edf6f8
yujinpan <1192878390@qq.com>
2022-09-28 18:14:44 +0800

    feat: add extension migrator interface and its tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,151 @@
-import { helloWorld } from './index';
+import fs from 'fs';
+import path from 'path';
 
-describe('helloWorld', () => {
-  it('should say', function () {
-    expect(helloWorld('Hello, World!')).toBe(undefined);
+import { extensionMigrator } from './index';
+
+const root = 'example-files';
+
+beforeEach(() => {
+  fs.mkdirSync(root);
+});
+
+afterEach(() => {
+  fs.rmdirSync(root, { recursive: true });
+});
+
+const writeFile = (filepath: string, data = '') => {
+  filepath = path.resolve(root, filepath);
+  fs.mkdirSync(path.dirname(filepath), { recursive: true });
+  fs.writeFileSync(filepath, data);
+};
+
+const readFile = (filepath: string) => {
+  return fs.readFileSync(path.resolve(root, filepath)).toString();
+};
+
+describe('extensionMigrator', () => {
+  it('should complete js/ts/jsx/tsx/vue', function () {
+    writeFile('example-js.js');
+    writeFile('example-ts.ts');
+    writeFile('example-jsx.jsx');
+    writeFile('example-tsx.tsx');
+    writeFile('example-vue.vue');
+    writeFile(
+      'index.js',
+      `
+import "example-js";
+import "example-ts";
+import "example-jsx";
+import "example-tsx";
+import "example-vue";
+`,
+    );
+
+    extensionMigrator({
+      files: [path.resolve(root, 'index.js')],
+      complete: true,
+    });
+    expect(readFile('index.js')).toBe(
+      `
+import "example-js.js";
+import "example-ts.ts";
+import "example-jsx.jsx";
+import "example-tsx.tsx";
+import "example-vue.vue";
+`,
+    );
+
+    extensionMigrator({
+      files: [path.resolve(root, 'index.js')],
+    });
+    expect(readFile('index.js')).toBe(
+      `
+import "example-js";
+import "example-ts";
+import "example-jsx";
+import "example-tsx";
+import "example-vue.vue";
+`,
+    );
+  });
+
+  it('should complete index.js/ts/jsx/tsx/vue', function () {
+    writeFile('example-js/index.js');
+    writeFile('example-ts/index.ts');
+    writeFile('example-jsx/index.jsx');
+    writeFile('example-tsx/index.tsx');
+    writeFile('example-vue/index.vue');
+    writeFile(
+      'index.js',
+      `
+import "example-js";
+import "example-ts";
+import "example-jsx";
+import "example-tsx";
+import "example-vue";
+`,
+    );
+
+    extensionMigrator({
+      files: [path.resolve(root, 'index.js')],
+      complete: true,
+    });
+    expect(readFile('index.js')).toBe(
+      `
+import "example-js/index.js";
+import "example-ts/index.ts";
+import "example-jsx/index.jsx";
+import "example-tsx/index.tsx";
+import "example-vue/index.vue";
+`,
+    );
+
+    extensionMigrator({
+      files: [path.resolve(root, 'index.js')],
+    });
+    expect(readFile('index.js')).toBe(
+      `
+import "example-js";
+import "example-ts";
+import "example-jsx";
+import "example-tsx";
+import "example-vue/index.vue";
+`,
+    );
+  });
+
+  it('should complete with alias', function () {
+    writeFile('src/index.js');
+    writeFile('src/example.js');
+    writeFile(
+      'index.js',
+      `
+import '@'
+import '@/example'
+    `,
+    );
+
+    extensionMigrator({
+      files: [path.resolve(root, 'index.js')],
+      complete: true,
+      alias: {
+        '@': root + '/src',
+      },
+    });
+    expect(readFile('index.js')).toBe(`
+import '@/index.js'
+import '@/example.js'
+    `);
+
+    extensionMigrator({
+      files: [path.resolve(root, 'index.js')],
+      alias: {
+        '@': root + '/src',
+      },
+    });
+    expect(readFile('index.js')).toBe(`
+import '@'
+import '@/example'
+    `);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,194 @@
-export function helloWorld(msg: string) {
-  // eslint-disable-next-line no-console
-  console.log(msg);
+import fs from 'fs';
+import glob from 'glob';
+import path from 'path';
+
+export type Options = {
+  files?: string[];
+  alias?: Record<string, string>;
+  extensions?: string[];
+  complete?:
+    | boolean
+    | {
+        js?: boolean;
+        jsx?: boolean;
+        ts?: boolean;
+        tsx?: boolean;
+        vue?: boolean;
+      };
+  log?: boolean;
+};
+
+export function extensionMigrator(options: Options = {}) {
+  const complete =
+    typeof options.complete === 'boolean'
+      ? {
+          js: options.complete,
+          jsx: options.complete,
+          ts: options.complete,
+          tsx: options.complete,
+          vue: options.complete,
+        }
+      : {
+          js: false,
+          jsx: false,
+          ts: false,
+          tsx: false,
+          vue: true,
+          ...options.complete,
+        };
+  const extensions = [
+    'js',
+    'ts',
+    'jsx',
+    'tsx',
+    'vue',
+    'scss',
+    'css',
+    ...(options.extensions || []),
+  ];
+  const files: string[] = options.files
+    ? options.files.map((item) => glob.sync(item)).flat()
+    : glob.sync(`src/*.+(${extensions.join('|')})`);
+
+  files.forEach((filepath) => {
+    const categoryImports = getCategoryImports(
+      fs.readFileSync(filepath).toString(),
+      extensions,
+    );
+    for (const extension in categoryImports) {
+      if (extension === 'none') {
+        for (const key in complete) {
+          if (complete[key]) {
+            // need complete
+            categoryImports.none.forEach((item) => {
+              writeFileImport(
+                filepath,
+                item,
+                tryFindFile(filepath, item, [key], options.alias),
+                options.log,
+              );
+            });
+          }
+        }
+      } else if (complete[extension]) {
+        // need complete
+        categoryImports[extension].forEach((item) => {
+          writeFileImport(
+            filepath,
+            item,
+            tryFindFile(filepath, item, extensions, options.alias),
+            options.log,
+          );
+        });
+      } else {
+        // need remove
+        categoryImports[extension].forEach((item) =>
+          writeFileImport(
+            filepath,
+            item,
+            item.replace(new RegExp(`(/index)?.${extension}$`), ''),
+            options.log,
+          ),
+        );
+      }
+    }
+  });
 }
 
-helloWorld('Hello, World!');
+function tryFindFile(
+  filepath: string,
+  importPath: string,
+  extensions: string[],
+  alias: Record<string, string> = {},
+) {
+  const extensionsPattern = `.+(${extensions.join('|')})`;
+  const filepathAbsolute = toAbsolutePath(filepath, importPath, alias);
+
+  let path: string = glob.sync(`${filepathAbsolute}${extensionsPattern}`)[0];
+  if (!path) {
+    path = glob.sync(`${filepathAbsolute}/index${extensionsPattern}`)[0];
+  }
+
+  return path ? importPath + path.replace(filepathAbsolute, '') : importPath;
+}
+
+function writeFileImport(
+  filepath: string,
+  oldImport: string,
+  newImport: string,
+  log?: boolean,
+) {
+  log &&
+    // eslint-disable-next-line no-console
+    console.log(`
+- filepath: ${filepath}
+- oldImport: ${oldImport}
+- newImport: ${newImport}
+  `);
+  const content = fs.readFileSync(filepath).toString();
+  fs.writeFileSync(filepath, content.replace(oldImport, newImport));
+}
+
+function toAbsolutePath(
+  filepath: string,
+  importPath: string,
+  alias: Record<string, string> = {},
+) {
+  for (const key in alias) {
+    const aliasPath = path.resolve(alias[key]);
+    if (importPath === key || importPath.startsWith(key)) {
+      return importPath.replace(key, aliasPath);
+    }
+  }
+  return path.resolve(path.dirname(filepath), importPath);
+}
+
+function getCategoryImports(content: string, extensions: string[]) {
+  const result: Record<string, string[]> = {
+    none: [],
+  };
+  const imports = getImports(content);
+  imports.forEach((item) => {
+    const find = extensions.find((extension) => item.endsWith('.' + extension));
+    if (find) {
+      if (!result[find]) result[find] = [];
+      result[find].push(item);
+    } else {
+      result.none.push(item);
+    }
+  });
+  return result;
+}
+
+/**
+ * 获取导入地址
+ * import {} from '...'
+ * import '...'
+ * require('...')
+ * import('...')
+ * @import('...')
+ * @import url('...')
+ */
+function getImports(content: string) {
+  return (
+    removeComments(content)
+      .match(
+        new RegExp(
+          '(from\\s+|import\\s+|require\\(|import\\(|@import\\s+(url\\()?)(\'|").+(\'|")',
+          'g',
+        ),
+      )
+      ?.map((item) => item.replace(/.*['"](.+)['"].*/, '$1')) || []
+  );
+}
+
+/**
+ * 移除注释代码
+ * - //
+ * - /*
+ * - *
+ * - <
+ */
+function removeComments(content: string) {
+  return content.replace(/^\s*(\/\/|\/\*|\*|<).*$/gm, '');
+}
